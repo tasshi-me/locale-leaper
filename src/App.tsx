28a0e31d88ce2fdb2fb764e88683cb0e6918044a
Masaharu TASHIRO <masatsr.kit@gmail.com>
2024-01-29 14:10:10 +0900

    chore: use GlobalStyle and remove CSS files

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,10 @@ import Timezones from "./components/Timezones";
 import Locales from "./components/Locales";
 import { ErrorBoundary } from "./components/ErrorBoundary";
 import { Header } from "./components/Header";
-import styled from "styled-components";
 import { Card } from "./components/Card";
 import { GlobalStyle } from "./components/GlobalStyle";
 
-const Component: React.FC = () => {
+const App: React.FC = () => {
   const [reloadRequired, setReloadRequired] = useState<boolean>(false);
 
   return (
@@ -42,14 +41,5 @@ const Component: React.FC = () => {
     </>
   );
 };
-
-const App = styled(Component)`
-  //width: 100%;
-  //height: 600px;
-  margin: 0 auto;
-  /*padding: 2rem;*/
-  padding: 0;
-  text-align: center;
-`;
 
 export default App;
